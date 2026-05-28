349e7c997742b793a0e6a1a98ec43f41f611750a
Elena Voronina <elenavoronix@gmail.com>
2026-05-28 09:35:01 +0600

    Add WorkoutTypeValidator

---
+++ src/main/java/com/fitness/validator/WorkoutTypeValidator.java
@@ -0,0 +1,31 @@
+package com.fitness.validator;
+
+import com.fitness.dao.interfaces.WorkoutTypeDao;
+import com.fitness.dto.workout_type_dto.WorkoutTypeCreateDto;
+import com.fitness.dto.workout_type_dto.WorkoutTypeUpdateDto;
+import com.fitness.exception.ValidationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WorkoutTypeValidator {
+
+    private final WorkoutTypeDao workoutTypeDao;
+
+    public WorkoutTypeValidator(WorkoutTypeDao workoutTypeDao) {
+        this.workoutTypeDao = workoutTypeDao;
+    }
+
+    public void validateCreateDto(WorkoutTypeCreateDto dto) {
+
+        if (workoutTypeDao.existsByTitle(dto.getTitle())) {
+            throw new ValidationException("workout type already exists");
+        }
+    }
+
+    public void validateUpdateDto(WorkoutTypeUpdateDto dto) {
+
+        if (workoutTypeDao.existsByTitleAndIdNot(dto.getTitle(), dto.getId())) {
+            throw new ValidationException("workout type already exists");
+        }
+    }
+}
